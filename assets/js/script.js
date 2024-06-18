dcaacb94970e1acf4bf959804da57de7f3db7bb9
Claudia Paniago <claudiacostapaniago@gmail.com>
2024-06-18 13:36:59 -0300

    projeto finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,22 @@ const relogio = setInterval(function time() {
   let sec = dateToday.getSeconds();
 
   if (hr < 10) hr = "0" + hr;
-
   if (min < 10) min = "0" + min;
-
   if (sec < 10) sec = "0" + sec;
 
   horas.textContent = hr;
   minutos.textContent = min;
   segundos.textContent = sec;
+
+  // Remove qualquer classe de período anterior
+  document.body.classList.remove("manha", "tarde", "noite");
+
+  // Determine o período do dia e adicione a classe correspondente
+  if (hr >= 6 && hr < 12) {
+    document.body.classList.add("manha");
+  } else if (hr >= 12 && hr < 18) {
+    document.body.classList.add("tarde");
+  } else {
+    document.body.classList.add("noite");
+  }
 });
